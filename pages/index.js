e8e670cc650cdcdda929a844394c80e30e9ddcd4
Pedro Candido <pcandido@dotz.com>
2021-08-01 10:33:03 -0300

    feat: reset css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import { Header } from '../src/components/common/Header'
+import { Header, Footer } from '../src/components/common'
 
 export default function Home() {
   return (
-    <>
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'column'
+    }}>
       <Header />
-    </>
+      <Footer />
+    </div>
   )
 }
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+import { normalize } from 'styled-normalize'
+
+// CSS Reset
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: ${({ theme }) => theme.fontFamily};
+  }
+
+    /* Full height layout */
+    html, body {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
